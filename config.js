73f19da080a46b83bd30de164045f65e5be510bd
divrawat <divrawat2001@gmail>
2024-09-21 11:59:53 +0530

    sf0ver5g

---
+++ config.js
@@ -0,0 +1,370 @@
+export const DOMAIN = 'http://localhost:3000';
+// export const DOMAIN = 'https://jinxchapters.online';
+
+export const DOMAIN_NAME = 'jinxchapters.online';
+
+export const IMAGES_SUBDOMAIN = 'https://images.mangatoons.online/jinx';
+export const IMAGES_SUBDOMAIN_LOGO = 'https://images.mangatoons.online';
+
+// export const MANGA_DESCRIPTION0 = `<a href="${DOMAIN}"><u><b>Vinland Saga</b></u></a>`;
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u><b>Jinx manga</b></u></a> is created by Mingwa, known for creating the popular series BJ Alex. The story follows Jin and X, two high school students whose friendship slowly evolves into something more. What sets this series apart is its captivating storyline, well-developed characters, and impressive art style.`;
+
+
+
+export const MANGA_NAME = 'Jinx';
+export const MANGA_AUTHOR = 'Mingwa';
+export const MANGA_RELEASE = '2022';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Fujino Ōmori';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Action, Adventure, Drama, Shonen, Fantasy, School';
+export const MANGA_TYPE = 'Manga';
+export const HEADER_MANGA_DESC = "Jinx is a BL (Boys' Love) manhwa by Mingwa, the creator of the well-known series BJ Alex. The story revolves around two high school students, Jin and X, who start off as friends but gradually develop a deeper relationship. The manga stands out for its engaging plot, well-crafted characters, and stunning artwork."
+
+export const CHAPTER_PREFIX = 'chapter';
+export const IMAGE_PREFIX = 'images/vinland-saga';
+export const NEXT_PREVIOUS_PREFIX = 'chapter';
+export const URL_PREFIX = 'chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/jinx-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] w-[65px] rounded-[45%] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'Jinx';
+export const NavbarName = "Jinx";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} ${MANGA_TYPE} online at ${DOMAIN}. ${HEADER_MANGA_DESC}`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "Jinx is a BL (Boys' Love) manhwa by Mingwa that centers on the evolving relationship between two high schoolers, Jin and X. Jin is a quiet, introverted student, while X is energetic and rebellious. Despite their differences, they grow closer as they navigate their personal struggles and emotions. The story highlights friendship, love, and the journey of self-discovery, while also showing the societal pressures and internal conflicts they face."
+    },
+    {
+        id: 2,
+        content: "Mingwa’s writing is engaging, with characters that feel authentic. The bond between Jin and X is portrayed realistically, and the supporting characters add richness to the story. The art enhances the experience with expressive character designs and detailed backgrounds, making Jinx a touching and memorable manhwa for fans of emotional narratives."
+    },
+    {
+        id: 3,
+        content: "The core of Jinx revolves around the growing bond between Jin and X. Jin, a reserved student, contrasts with X’s bold and carefree nature. Their initial interactions are marked by tension, but as they spend more time together, a deeper connection forms. The story effectively captures their shift from friendship to something more intimate."
+    },
+    {
+        id: 4,
+        content: "Mingwa portrays Jin and X’s relationship with emotional depth. As they confront their feelings, they also face societal expectations and their own insecurities. Jin’s quiet internal struggles and X’s seemingly carefree but guarded demeanor create a dynamic that keeps readers engaged. Both characters are flawed yet relatable, growing through their experiences."
+    },
+
+    {
+        id: 5,
+        content: "The challenges they face go beyond their relationship. Jinx also explores broader issues like the pressure to conform and the fear of vulnerability. Mingwa skillfully integrates these themes, making the story more than just a romance—it’s also about identity and acceptance. As Jin and X navigate their emotions, readers are taken on a journey of growth and self-awareness."
+    },
+
+    {
+        id: 6,
+        content: "A major strength of Jinx is its character development. Jin’s introversion and X’s lively personality are explored in depth, revealing their individual fears and insecurities. Their relationship grows naturally, with believable interactions that add to the authenticity of the story. The side characters further enrich the plot, providing support and adding layers to the narrative."
+    },
+    {
+        id: 7,
+        content: "The artwork is another standout feature of Jinx. Mingwa’s detailed and expressive style beautifully captures the emotions of the characters, enhancing the storytelling. The backgrounds and character designs create a vivid atmosphere, complementing both the emotional and lighthearted moments of the story."
+    },
+
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Jinx Chapter 50', href: `${DOMAIN}/chapter-50` },
+    { text: 'Jinx Chapter 10', href: `${DOMAIN}/chapter-10` },
+    { text: 'Jinx Chapter 54', href: `${DOMAIN}/chapter-54` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    // { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    // { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    // { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+
+
+
+
+export const chaptersData =
+
+    [
+        {
+            "chapterNumber": "1",
+            "numImages": 96
+        },
+        {
+            "chapterNumber": "2",
+            "numImages": 61
+        },
+        {
+            "chapterNumber": "3",
+            "numImages": 50
+        },
+        {
+            "chapterNumber": "4",
+            "numImages": 55
+        },
+        {
+            "chapterNumber": "5",
+            "numImages": 67
+        },
+        {
+            "chapterNumber": "6",
+            "numImages": 55
+        },
+        {
+            "chapterNumber": "7",
+            "numImages": 61
+        },
+        {
+            "chapterNumber": "8",
+            "numImages": 8
+        },
+        {
+            "chapterNumber": "9",
+            "numImages": 55
+        },
+        {
+            "chapterNumber": "10",
+            "numImages": 49
+        },
+        {
+            "chapterNumber": "11",
+            "numImages": 49
+        },
+        {
+            "chapterNumber": "12",
+            "numImages": 49
+        },
+        {
+            "chapterNumber": "13",
+            "numImages": 49
+        },
+        {
+            "chapterNumber": "14",
+            "numImages": 55
+        },
+        {
+            "chapterNumber": "15",
+            "numImages": 66
+        },
+        {
+            "chapterNumber": "16",
+            "numImages": 49
+        },
+        {
+            "chapterNumber": "17",
+            "numImages": 54
+        },
+        {
+            "chapterNumber": "18",
+            "numImages": 49
+        },
+        {
+            "chapterNumber": "19",
+            "numImages": 55
+        },
+        {
+            "chapterNumber": "20",
+            "numImages": 48
+        },
+        {
+            "chapterNumber": "21",
+            "numImages": 49
+        },
+        {
+            "chapterNumber": "22",
+            "numImages": 61
+        },
+        {
+            "chapterNumber": "23",
+            "numImages": 57
+        },
+        {
+            "chapterNumber": "24",
+            "numImages": 63
+        },
+        {
+            "chapterNumber": "25",
+            "numImages": 63
+        },
+        {
+            "chapterNumber": "26",
+            "numImages": 63
+        },
+        {
+            "chapterNumber": "27",
+            "numImages": 71
+        },
+        {
+            "chapterNumber": "28",
+            "numImages": 7
+        },
+        {
+            "chapterNumber": "29",
+            "numImages": 70
+        },
+        {
+            "chapterNumber": "30",
+            "numImages": 7
+        },
+        {
+            "chapterNumber": "31",
+            "numImages": 71
+        },
+        {
+            "chapterNumber": "32",
+            "numImages": 15
+        },
+        {
+            "chapterNumber": "33",
+            "numImages": 60
+        },
+        {
+            "chapterNumber": "34",
+            "numImages": 48
+        },
+        {
+            "chapterNumber": "35",
+            "numImages": 69
+        },
+        {
+            "chapterNumber": "36",
+            "numImages": 19
+        },
+        {
+            "chapterNumber": "37",
+            "numImages": 82
+        },
+        {
+            "chapterNumber": "37a",
+            "numImages": 61
+        },
+        {
+            "chapterNumber": "38",
+            "numImages": 24
+        },
+        {
+            "chapterNumber": "39",
+            "numImages": 19
+        },
+        {
+            "chapterNumber": "40",
+            "numImages": 9
+        },
+        {
+            "chapterNumber": "40a",
+            "numImages": 2
+        },
+        {
+            "chapterNumber": "41",
+            "numImages": 38
+        },
+        {
+            "chapterNumber": "42",
+            "numImages": 17
+        },
+        {
+            "chapterNumber": "43",
+            "numImages": 20
+        },
+        {
+            "chapterNumber": "44",
+            "numImages": 19
+        },
+        {
+            "chapterNumber": "45",
+            "numImages": 19
+        },
+        {
+            "chapterNumber": "46",
+            "numImages": 17
+        },
+        {
+            "chapterNumber": "47",
+            "numImages": 21
+        },
+        {
+            "chapterNumber": "48",
+            "numImages": 21
+        },
+        {
+            "chapterNumber": "49",
+            "numImages": 19
+        },
+        {
+            "chapterNumber": "50",
+            "numImages": 18
+        },
+        {
+            "chapterNumber": "51",
+            "numImages": 19
+        },
+        {
+            "chapterNumber": "52",
+            "numImages": 19
+        },
+        {
+            "chapterNumber": "53",
+            "numImages": 4
+        },
+        {
+            "chapterNumber": "53a",
+            "numImages": 33
+        },
+        {
+            "chapterNumber": "53b",
+            "numImages": 9
+        },
+        {
+            "chapterNumber": "53c",
+            "numImages": 11
+        },
+        {
+            "chapterNumber": "54",
+            "numImages": 39
+        }
+    ]
+
+
+
+
+
+export const last5chapters = [
+    {
+        "chapterNumber": "53",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "53a",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "53b",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "53c",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 39
+    }
+]
